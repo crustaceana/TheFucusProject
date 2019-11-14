6488f69dab920548a4802555ed9ae92082f0e0dc
sam per <prnsml@gmail.com>
2019-11-14 14:01:23 +0100

    heatmaps ready

--- scripts/get_afs.R
+++ scripts/get_afs.R
@@ -20,6 +20,8 @@ lapply(basename(.packagesdev), require, character.only=TRUE)
 option_list = list(
   make_option(c("-C", "--csv"), type="character", default=NULL,
               help="input CSV genotype table", metavar="character"),
+  make_option(c("-E", "--extension"), type="character", default=NULL,
+              help="which format to save the figures, e.g., .svg or .pdf", metavar="character"),
   make_option(c("-T", "--outxt"), type="character", default=NULL,
               help="output text file with list of fixed variants in the outgroups", metavar="character"),
   make_option(c("-O", "--outgroups"), type="character", default=NULL,
@@ -30,14 +32,20 @@ opt_parser = OptionParser(option_list=option_list,
                           epilogue = "Example: Rscript scripts/get_afs.R -C data/Clone_GATK_final_loci_edit.csv -T fixed_var_outgroup.txt -O KRI_sex TJA_sex")
 opt = parse_args(opt_parser)
 
-if (is.null(opt$csv) | is.null(opt$outxt) | is.null(opt$outgroups)) {
+if (is.null(opt$csv) | is.null(opt$extension) | is.null(opt$outgroups)) {
   print_help(opt_parser)
   stop("All arguments must be supplied.\n", call.=FALSE)
 }
 
-# csv_call = read.csv("data/Clone_GATK_final_loci_edit.csv")
+# csv_gt = read.csv("data/Clone_GATK_final_loci_edit.csv")
 cat("Reading input", opt$csv, "...\n")
-csv_call = read.csv(opt$csv)
+csv_gt = read.csv(opt$csv)
+# fl_line = list.files(path = "data", pattern = "5_lineages", full.names = TRUE)
+fl_line = list.files(path = "data", pattern = opt$lineages, full.names = TRUE)
+csv_line = read.csv(fl_line)
+if (identical(csv_gt$Pop, csv_line$Pop)) {
+  csv_call = cbind(csv_gt[,1:2], Line=csv_line$Line, csv_gt[, 3:ncol(csv_gt)])
+}
 # csv_call[1:5,1:10]
 ########################
 ######### TEST #########
@@ -47,9 +55,7 @@ dir.create("docs")
 # file.create("docs/max_val_allele1.txt")
 # outg = c("KRI_sex", "TJA_sex")
 outg = strsplit(opt$outgroups, split = " ")[[1]]
-# csv_call = csv_call[,1:10]
-
-scaf_id = colnames(csv_call[!grepl(pattern = "Ind|Pop|A2", x = colnames(csv_call))])
+scaf_id = colnames(csv_call[!grepl(pattern = "Ind|Pop|Line|A2", x = colnames(csv_call))])
 
 # oldtxt = read.table("fixed_var_outgroup.txt")
 # newtxt = read.table("docs/fixed_var_outgroup.txt")
@@ -67,7 +73,8 @@ get_afs = function(csv) {
   id_fix_csv = lapply(scaf_id, function(x) {
     cat("Calculating frequency of", x, "...\n")
     one_scaf = csv[, colnames(csv)[grepl(pattern = x, x = colnames(csv))]]
-    one_scaf_pop = cbind(Pop = csv[,2], one_scaf)
+    # group by Pop or Line
+    one_scaf_pop = cbind(Pop = csv[,"Line"], one_scaf)
     one_freq_tot = data.frame(table(one_scaf_pop$Pop, one_scaf_pop[, 2]),
                               Tot = data.frame(table(one_scaf_pop$Pop))[,2]*2)
     two_freq_tot = data.frame(table(one_scaf_pop$Pop, one_scaf_pop[, 3]),
@@ -101,10 +108,14 @@ get_afs = function(csv) {
     one_af = mutate(t4, AF = Count/Tot)
     colnames(one_af) = c("Pop", "Allele", "Count", "Tot", paste0("AF_", x))
     # If AF of outgroups is below 1 remove the AF value
+    if (sum(one_af[,'Pop'] %in% "outg") > 0) {
+      outg = "outg"
+    }
     outg_fr = one_af[which(one_af$Pop %in% outg), dim(one_af)[2]]
     outg_vc = rep(1,length(outg_fr))
     outg_filter = outg_vc[outg_fr == 1 | outg_fr == 0]
-    if (sum(outg_filter) == 4) {
+    # change sum to 2 if using lineages
+    if (sum(outg_filter) == 2) {
       # Add column to be able to filter out dataframes without the extra column
       one_af$Homo_outg = 1
       # Write variant to txt output
@@ -117,31 +128,9 @@ get_afs = function(csv) {
 }
 afs_list = get_afs(csv = csv_call)
 # afs_list[[10]]
-# colSums(afs_list[[10]][, c("Count", "Tot")])
+# colSums(afs_list[[101]][, c("Count", "Tot")])
 afs_list_homo_outg = Filter(function(x) ncol(x)==6, afs_list)
 cat("The number of polarised variants is", length(afs_list_homo_outg), "\n")
-
-# lapply(seq_along(afs_list_homo_outg), function(s) {
-#   colSums(afs_list_homo_outg[[s]][, c("Count", "Tot")])
-# })
-# afs_list_homo_outg[[533]]
-
-# scaf_call = csv_call[, colnames(csv_call)[grepl(pattern = "scaffold780_58219", x = colnames(csv_call))]]
-# scaf_pop_call = cbind(Pop = csv_call[,2], scaf_call)
-# t1 = data.frame(table(scaf_pop_call$Pop, scaf_pop_call[, 2]),
-#                 Tot = data.frame(table(scaf_pop_call$Pop))[,2]*2)
-# t2 = data.frame(table(scaf_pop_call$Pop, scaf_pop_call[, 3]),
-#                 Tot = data.frame(table(scaf_pop_call$Pop))[,2]*2)
-# str(t1)
-# t3 = data.frame(Pop = t1[t1$Var2==1,]$Var1, Allele = t1[t1$Var2==1,]$Var2,
-#                 Count = t1[t1$Var2==1, "Freq"] + t2[t2$Var2==1, "Freq"], Tot = t1[t1$Var2==1, "Tot"])
-# t3 = data.frame(Pop = t1[t1$Var2==1,]$Var1, Allele = t1[t1$Var2==1,]$Var2,
-#                 Count = t1[t1$Var2==1, "Freq"], Tot = t1[t1$Var2==1, "Tot"])
-# colnames(t1) = colnames(t3)
-# colnames(t2) = colnames(t3)
-# t4 = rbind(t3, t1[t1$Allele==1,])
-# t4 = rbind(t3, t2[t2$Allele==2,])
-# colSums(t4[,-1:-2])
 
 # get_daf function selects which allele is the derived since the ancestral allele has frequency = 1 in the outgroups
 get_daf = function(df) {
@@ -153,7 +142,23 @@ get_daf = function(df) {
   }
   return(der_df)
 }
-der_afs = lapply(afs_list_homo_outg, function(x) get_daf(df = x))
+# get_daf_line function works the same as get_daf but with lineages
+get_daf_line = function(df) {
+  outg_df = df[df$Pop=="outg", c("Count", "Tot")]
+  if (outg_df[1,1]==outg_df[1,2]) {
+    der_df = df[df$Allele==2, ]
+  } else if (outg_df[2,1]==outg_df[2,2]) {
+    der_df = df[df$Allele==1, ]
+  }
+  return(der_df)
+}
+if (sum(csv_call[,'Line'] %in% "outg") > 0) {
+  outg = "outg"
+  der_afs = lapply(afs_list_homo_outg, function(x) get_daf_line(df = x))
+} else {
+  der_afs = lapply(afs_list_homo_outg, function(x) get_daf(df = x))
+}
+
 # der_afs[[140]]
 
 # Bind dataframes by columns and select only columns for the derived frequency and the column for the population names
@@ -170,8 +175,13 @@ fin_var_by_pop = t(fin_daf_scaf)
 # table(as.data.frame(fin_var_by_pop)[,4])
 
 # Remove frequency of the derived allele of the outgroups because = 0
-fin_var_by_pop = fin_var_by_pop[, -which(colnames(fin_var_by_pop) == outg[1])]
-fin_var_by_pop = fin_var_by_pop[, -which(colnames(fin_var_by_pop) == outg[2])]
+if (sum(csv_call[,'Line'] %in% "outg") > 0) {
+  outg = "outg"
+  fin_var_by_pop = fin_var_by_pop[, -which(colnames(fin_var_by_pop) == outg)]
+} else {
+  fin_var_by_pop = fin_var_by_pop[, -which(colnames(fin_var_by_pop) == outg[1])]
+  fin_var_by_pop = fin_var_by_pop[, -which(colnames(fin_var_by_pop) == outg[2])]
+}
 
 # Find varinats with non-zero frequency in all populations
 all_shared = fin_var_by_pop[rowSums(fin_var_by_pop > 0) == ncol(fin_var_by_pop), ]
@@ -200,21 +210,36 @@ intersect(rownames(sex_shared), rownames(clo_shared))
 # head(sis_lin)
 # sis_shared = sis_lin[rowSums(sis_lin > 0) == ncol(sis_lin), ]
 
-dir.create("TheFucusProject/figures/DJU_sis_clone")
-lapply(colnames(fin_var_by_pop), function(p) {
-  pl = ggplot(data = as.data.frame(fin_var_by_pop), aes(x = DJU_sis_clone, y = as.data.frame(fin_var_by_pop)[,p])) +
-    # geom_point(aes(x = DJU_sis_clone, y = as.data.frame(fin_var_by_pop)[,p])) +
-    stat_sum(aes(size = factor(..n..)), geom = "point") +
-    labs(y=p) +
-    ylim(c(0,1))
-  cat("Saving figure TheFucusProject/figures/DJU_sis_clone/DJU_sis_clone_vs_", p, "_DAF.svg ...\n")
-  ggsave(file=paste0("TheFucusProject/figures/DJU_sis_clone/DJU_sis_clone_vs_", p, "_DAF.svg"),
-         plot=pl, width=10, height=8)
+df_grid = expand.grid(popx = seq(from=0,to = 1,by = 0.1), popy = seq(from=0,to = 1,by = 0.1))
+pl_comb = combn(x = 1:ncol(fin_var_by_pop), m = 2)
+afs_2d_heat = function(dataf, n_comb) {
+  df = as.data.frame(dataf)[, pl_comb[, n_comb]]
+  df = round(df, 1)
+  df$Count = apply(df, MARGIN = 1, FUN = function(x) {
+    nrow(df[df[, 1]==x[1] & df[, 2]==x[2],])
+  })
+  dfu = unique(df)
+  colnames(dfu) = c("popx", "popy", "Count")
+  df_merge = merge(dfu, df_grid, by = c("popx", "popy"), all = TRUE)
+  df_merge[is.na(df_merge)] = 0
+  cat("Heatmapping", colnames(dataf)[pl_comb[1, n_comb]], "and", colnames(dataf)[pl_comb[2, n_comb]], "...\n")
+  one_fig = ggplot(df_merge, aes(x = popx, y = popy)) +
+    geom_tile(data = subset(df_merge, Count!=0), aes(fill = Count)) +
+    geom_tile(data = subset(df_merge,  Count==0), aes(colour = "0"),
+              linetype = 0, fill = "#330033") +
+    scale_fill_continuous(type = "viridis") +
+    labs(x = colnames(dataf)[pl_comb[1, n_comb]],
+         y = colnames(dataf)[pl_comb[2, n_comb]]) +
+    theme(legend.title = element_blank())
+  return(one_fig)
+}
+# ext = ".svg"
+ext = opt$extension
+lapply(1:ncol(pl_comb), function(x) {
+  cat("Saving heatmap",
+      paste0("TheFucusProject/figures/heatmap_der_", colnames(fin_var_by_pop)[pl_comb[1, x]], "_VS_", colnames(fin_var_by_pop)[pl_comb[2, x]], ext),
+      "...\n")
+  ggsave(filename = paste0("TheFucusProject/figures/heatmap_der_", colnames(fin_var_by_pop)[pl_comb[1, x]],
+                           "_VS_", colnames(fin_var_by_pop)[pl_comb[2, x]], ext),
+         plot = afs_2d_heat(dataf = fin_var_by_pop, n_comb = x), width = 10, height = 8)
 })
-# table(as.data.frame(fin_var_by_pop)[,"DJU_sis_clone"])
-# table(as.data.frame(fin_var_by_pop)[,"VALvesclone"])
-# lapply(1:ncol(fin_var_by_pop), function(x) {
-#   ggplot(data = as.data.frame(fin_var_by_pop)) +
-#     geom_histogram(aes(x = as.data.frame(fin_var_by_pop)[, x]))
-# })
-
